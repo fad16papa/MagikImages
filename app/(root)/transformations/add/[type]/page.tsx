deaeee17b622f44e97f32797150253bbdf821095
fad16papa <fad16papa@gmail.com>
2024-04-12 17:15:14 +0800

    Created an Header for forms

--- app/(root)/transformations/add/[type]/page.tsx
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -1,7 +1,12 @@
+import Header from "@/components/shared/Header";
 import React from "react";
+import { transformationTypes } from "@/constants";
 
-const AddTransformationTypePage = () => {
-  return <div>AddTransformationTypePage</div>;
+const AddTransformationTypePage = ({ params: { type } }: SearchParamProps) => {
+  const transformation = transformationTypes[type];
+  return (
+    <Header title={transformation.title} subtitle={transformation.subTitle} />
+  );
 };
 
 export default AddTransformationTypePage;
