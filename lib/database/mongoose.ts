b788b32d8d8108999fd8c742963d18e6c08ded23
fad16papa <fad16papa@gmail.com>
2024-04-12 15:52:40 +0800

    Commit changes

--- lib/database/mongoose.ts
+++ lib/database/mongoose.ts
@@ -10,7 +10,10 @@ interface MongooseConnection {
 let cached: MongooseConnection = (global as any).mongoose;
 
 if (!cached) {
-  cached = (global as any).mongoose = { conn: null, promise: null };
+  cached = (global as any).mongoose = {
+    conn: null,
+    promise: null,
+  };
 }
 
 export const connectToDatabase = async () => {
@@ -21,7 +24,7 @@ export const connectToDatabase = async () => {
   cached.promise =
     cached.promise ||
     mongoose.connect(MONGODB_URL, {
-      dbName: "magikimages",
+      dbName: "imaginify",
       bufferCommands: false,
     });
 
