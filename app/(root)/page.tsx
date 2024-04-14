0f3750c3e5f4e5839618b74ddc0f6946a285d8b3
fad16papa <fad16papa@gmail.com>
2024-04-15 00:47:04 +0800

    Commit changes

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -14,9 +14,7 @@ const Home = async ({ searchParams }: SearchParamProps) => {
   return (
     <>
       <section className="home">
-        <h1 className="home-heading">
-          Creat Your Vision with Magik Images
-        </h1>
+        <h1 className="home-heading">Creat Your Vision with Magik Images</h1>
         <ul className="flex-center w-full gap-20">
           {navLinks.slice(1, 5).map((link) => (
             <Link
